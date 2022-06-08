bc7e80127fec5da974297b6ac118e26aaea2e2c2
Bob Hyman <bob.hyman@gmail.com>
2022-06-07 20:21:27 -0400

    missing checkin!

--- touchup2.sh
+++ touchup2.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env -S "bash"
 
-hostname skserv
 adduser --add_extra_groups skuser
 adduser -q skuser sudo
 adduser -q skuser adm
@@ -9,12 +8,37 @@ adduser -q skuser gpio
 
 # application tailoring
 
+# enable nmea2000 canbus
+
+cat >> '/boot/config.txt' <<EOF
+enable_uart=1
+dtparam=i2c_arm=on
+dtparam=spi=on
+dtoverlay=mcp2515-can0,oscillator=16000000,interrupt=25
+dtoverlay=spi-bcm2835-overlay
+EOF
+
+cat > /etc/systemd/system/socketcan-interface.service <<EOF
+[Unit]
+Description=SocketCAN interface can0 with a baudrate of 250000
+[Service]
+Type=oneshot
+RemainAfterExit=yes
+ExecStart=/sbin/ip link set can0 type can bitrate 250000 ; /sbin/ifconfig can0 up
+ExecReload=/sbin/ifconfig can0 down ; /sbin/ip link set can0 type can bitrate 250000 ; /sbin/ifconfig can0 up
+ExecStop=/sbin/ifconfig can0 down
+[Install]
+WantedBy=multi-user.target
+EOF
+
+chmod 644 /etc/systemd/system/socketcan-interface.service
+
 # prepare to get nodejs (16, the prereq for SignalK)
 
 curl -fsSL https://deb.nodesource.com/setup_16.x | sudo bash -
 
 # prepare to get grafana
-# and grafna
+
 wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
 
 echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
@@ -25,9 +49,12 @@ apt update
 
 apt install -y \
     mtr-tiny \
+    fail2ban \
     cockpit \
+    can-utils \
     nodejs \
     grafana
+
 
 npm install --location=global npm@latest
 
@@ -41,15 +68,11 @@ tar xvfz influxdb-1.8.10_linux_arm64.tar.gz
 cd influxdb-1.8.10-1
 rsync -arv --mkpath ./ /
 popd
-rmdir -rf temp
+rm -rf temp
+
 
 ### Start services
 
-systemctl enable influxdb
-systemctl enable grafana
-
+systemctl enable socketcan-interface influxdb grafana
 
 echo "You will have to run 'sudo signalk-server-setup' to complete config for signal K and its service."
-
-
-
